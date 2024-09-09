0fd98320a52e617ff8530ba5d40c0d10bc8162dd
misbahabroruddin <misbahabroruddin@gmail.com>
2024-09-09 14:01:03 +0700

    style: change border color

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -16,7 +16,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-neutral-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
+          "flex h-9 w-full rounded-md border border-black-09 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
           className,
         )}
         ref={ref}
@@ -42,7 +42,7 @@ const InputWithIcon = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            "ring-offset-black-9 border-black-09 focus-visible:ring-black-08 flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
+            "ring-offset-black-9 flex h-10 w-full rounded-md border border-black-09 bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black-08 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
             icon ? "relative w-full pl-9" : "",
             className,
           )}
@@ -60,7 +60,7 @@ export const InputFileImport = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <label
         htmlFor="file_import"
-        className="border-black-08 relative flex h-10 w-32 cursor-pointer items-center justify-center rounded border bg-background px-4 py-[9.5] transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+        className="relative flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-black-08 bg-background px-4 py-[9.5] transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 lg:h-10 lg:w-32 lg:rounded"
       >
         {props.disabled ? (
           <div className="flex gap-2">
@@ -69,9 +69,9 @@ export const InputFileImport = React.forwardRef<HTMLInputElement, InputProps>(
           </div>
         ) : (
           <>
-            <div className="flex gap-2">
-              <ImportIcon color="black" />
-              <span>Import</span>
+            <div className="flex items-center gap-2">
+              <ImportIcon className="h-6 w-6" color="black" />
+              <span className="hidden lg:block">Import</span>
             </div>
             <input
               type="file"
