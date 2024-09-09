bdaf44891f7b2928d4f6786c9cfa4725c8a8cdcd
misbahabroruddin <misbahabroruddin@gmail.com>
2024-09-09 09:34:54 +0700

    feat: setup root page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Suspense } from "react";
 
 import "./globals.css";
 import { metadataConfig } from "@/config/metadata";
 import { Toaster } from "@/components/ui/sonner";
 import QueryProvider from "@/lib/providers/query-provider";
 import { SessionProvider } from "@/lib/providers/session-provider";
+import { SidebarProvider } from "@/lib/providers/sidebar-provider";
+import { Spinner } from "@/components/ui/spinner";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -26,7 +29,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className} suppressHydrationWarning>
         <SessionProvider>
-          <QueryProvider>{children}</QueryProvider>
+          <QueryProvider>
+            <SidebarProvider>
+              <Suspense
+                fallback={
+                  <div className="grid h-screen w-screen place-items-center">
+                    <Spinner />
+                  </div>
+                }
+              >
+                {children}
+              </Suspense>
+            </SidebarProvider>
+          </QueryProvider>
         </SessionProvider>
         <Toaster position="top-center" richColors theme="light" />
       </body>
