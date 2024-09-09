ae68b3f07d8c36c1491fcade8e2da83582265c7f
misbahabroruddin <misbahabroruddin@gmail.com>
2024-09-09 09:43:57 +0700

    feat: create role id and app id at sessio context provider

--- src/lib/providers/session-provider.tsx
+++ src/lib/providers/session-provider.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React, { createContext, useState, ReactNode, useEffect } from "react";
 
 interface SessionType {
   token: string | undefined;
   refreshToken: string | undefined;
   expiredAt: string | Date | undefined;
+  roleId: string | undefined;
+  appId: string | undefined;
 }
 
 export interface SessionContextType {
@@ -13,6 +16,8 @@ export interface SessionContextType {
   setToken: (token: string | undefined) => void;
   setRefreshToken: (refreshToken: string | undefined) => void;
   setExpiredAt: (expiredAt: string | undefined) => void;
+  setRole: (roleId: string | undefined) => void;
+  setApp: (appId: string | undefined) => void;
   signOut: () => void;
 }
 
@@ -25,13 +30,16 @@ const initialValues: SessionType = {
   token: undefined,
   refreshToken: undefined,
   expiredAt: undefined,
+  roleId: undefined,
+  appId: undefined,
 };
 
 export const SessionProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  // const [token, setAccessToken] = useState(getInitialState);
   const [session, setSession] = useState<SessionType>(initialValues);
+
+  const router = useRouter();
 
   const setToken = (token: string | undefined) => {
     setSession((prev) => ({ ...prev, token }));
@@ -48,6 +56,16 @@ export const SessionProvider: React.FC<{ children: ReactNode }> = ({
     if (expiredAt) localStorage.setItem("expiredAt", expiredAt!);
   };
 
+  const setRole = (roleId: string | undefined) => {
+    setSession((prev) => ({ ...prev, roleId }));
+    if (roleId) localStorage.setItem("role", roleId!);
+  };
+
+  const setApp = (appId: string | undefined) => {
+    setSession((prev) => ({ ...prev, appId }));
+    if (appId) localStorage.setItem("app", appId!);
+  };
+
   const signOut = () => {
     if (typeof window !== "undefined") {
       setSession(initialValues);
@@ -58,15 +76,17 @@ export const SessionProvider: React.FC<{ children: ReactNode }> = ({
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
+    const app = localStorage.getItem("app");
 
-    if (token) {
+    if (token && role && app) {
       setToken(token!);
+      setRole(role!);
+      setApp(app!);
     } else {
-      window.location.href = "https://sso.dev-unsia.id/backend/login";
+      window.location.href = `${process.env.NEXT_PUBLIC_SSO_BASE_URL}/backend/login`;
     }
   }, [session.token]);
-
-  // useEffect(() => {}, []);
 
   return (
     <SessionContext.Provider
@@ -75,6 +95,8 @@ export const SessionProvider: React.FC<{ children: ReactNode }> = ({
         setToken,
         setRefreshToken,
         setExpiredAt,
+        setRole,
+        setApp,
         signOut,
       }}
     >
