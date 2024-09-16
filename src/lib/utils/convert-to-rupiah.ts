7566a778c1a76802a3230617f0161b2fafd38b99
misbahabroruddin <misbahabroruddin@gmail.com>
2024-09-16 08:57:33 +0700

    feat: create util for convert to rupiah

---
+++ src/lib/utils/convert-to-rupiah.ts
@@ -0,0 +1,7 @@
+export const convertToRupiah = (data: string) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(parseInt(data));
+};
